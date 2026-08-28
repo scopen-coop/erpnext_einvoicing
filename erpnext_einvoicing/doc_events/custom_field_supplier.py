9898aec4da39c921f91acaf7c2692ed84906498f
Nico <nicolas.lavoine@scopen.fr>
2026-08-28 10:55:07 +0200

    Fix: align iso-16 with v16 cosmetic differences

--- erpnext_einvoicing/doc_events/custom_field_supplier.py
+++ erpnext_einvoicing/doc_events/custom_field_supplier.py
@@ -10,10 +10,8 @@ TARGET_DOCTYPE = "eThirdParty"
 def on_create(doc, method=None):
 	if doc.dt != ORIGIN_DOCTYPE:
 		return
-
 	if doc.reqd != 1:
 		return
-
 	cf = frappe.new_doc("Custom Field")
 	cf.dt = TARGET_DOCTYPE
 	cf.fieldname = doc.fieldname
@@ -26,7 +24,6 @@ def on_create(doc, method=None):
 	cf.description = doc.description
 	cf.hidden = doc.hidden
 	cf.read_only = doc.read_only
-
 	try:
 		cf.insert(ignore_permissions=True)
 		frappe.db.commit()
@@ -38,17 +35,14 @@ def on_create(doc, method=None):
 def on_update(doc, method=None):
 	if doc.dt != ORIGIN_DOCTYPE:
 		return
-
 	mirror_name = frappe.db.get_value("Custom Field", {"dt": TARGET_DOCTYPE, "fieldname": doc.fieldname})
 	if not mirror_name:
 		if doc.reqd == 1:
 			on_create(doc, method)
 		return
-
 	if doc.reqd != 1:
 		on_delete(doc, method)
 		return
-
 	cf = frappe.get_doc("Custom Field", mirror_name)
 	cf.label = doc.label
 	cf.fieldtype = doc.fieldtype
@@ -65,10 +59,8 @@ def on_update(doc, method=None):
 def on_delete(doc, method=None):
 	if doc.dt != ORIGIN_DOCTYPE:
 		return
-
 	mirror_name = frappe.db.get_value("Custom Field", {"dt": TARGET_DOCTYPE, "fieldname": doc.fieldname})
 	if not mirror_name:
 		return
-
 	frappe.delete_doc("Custom Field", mirror_name, ignore_permissions=True)
 	frappe.db.commit()
--- erpnext_einvoicing/erpnext_einvoicing/doctype/ethirdparty/test_ethirdparty.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Scopen and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TesteThirdParty(FrappeTestCase):
-	pass
